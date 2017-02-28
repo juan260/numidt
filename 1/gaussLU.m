f1fa97d57363a4bb41f6570173378c56715a8afc
juan260 <superjuan260@hotmail.com>
2017-02-28 13:29:16 +0100

    helonadf

--- 1/gaussLU.m
+++ 1/gaussLU.m
@@ -1,8 +1,14 @@
-function [L, U] = gaussLU(A)
+function [L, U, E] = gaussLU(A)
     U=A;
     [n,m]=size(A);
+    if n ~= m
+        error('La matriz no es cuadrada')
+    end
     L=eye(n);
     for k = 1:n-1
+        if U(k,k) == 0 
+            error('Error: el elemento %d,%d es 0', k, k)
+        end
         for i = k+1:n
            L(i,k) = U(i,k)/U(k,k);
            for j = k:n
@@ -10,4 +16,6 @@ function [L, U] = gaussLU(A)
            end
         end
     end
+    
+    E=max(max(abs(A - U*L)));
 end
